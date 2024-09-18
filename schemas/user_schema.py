edb609c0e8fc9dfdb5a126c78f6dbdebd8291991
Alejo Valentin Saavedra <jaehaenar@DESKTOP-FD111KH>
2024-09-18 13:38:12 -0300

    SCRUM-66 add branch

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -5,4 +5,6 @@ class User(BaseModel):
        Example of Field and BaseModel""" #Modificar
     id: int = Field(description="Unique integer that specifies this user.")
     name: str = Field(min_length=1,max_length=100,description="Name of the user.")
+    
+class
 
