d5a02df7b2a726ebd4d801156aca6f3580f27dcb
LucianoRojo <lucianorojo064@mi.unc.edu.ar>
2024-09-21 19:46:10 -0300

    Agregados setters de game

--- querys/game_queries.py
+++ querys/game_queries.py
@@ -30,4 +30,30 @@ def get_game_min_players(db: Session, id_game: int):
 def get_game_password(db: Session, id_game: int):
     return db.query(Game).filter(Game.id == id_game).first().password
 
+def set_game_state(db: Session, id_game: int, state: str):
+    db.query(Game).filter(Game.id == id_game).update({Game.state: state})
+    db.commit()
+
+def set_game_turn(db: Session, id_game: int, turn: int):
+    db.query(Game).filter(Game.id == id_game).update({Game.turn: turn})
+    db.commit()
+
+def set_game_host(db: Session, id_game: int, host: str):
+    db.query(Game).filter(Game.id == id_game).update({Game.host: host})
+    db.commit()
+
+def set_game_max_players(db: Session, id_game: int, max_players: int):
+    db.query(Game).filter(Game.id == id_game).update({Game.max_players: max_players})
+    db.commit()
+
+def set_game_min_players(db: Session, id_game: int, min_players: int):
+    db.query(Game).filter(Game.id == id_game).update({Game.min_players: min_players})
+    db.commit()
+
+def set_game_password(db: Session, id_game: int, password: str):
+    db.query(Game).filter(Game.id == id_game).update({Game.password: password})
+    db.commit()
+
+
+
 #Alguna para el timer?
