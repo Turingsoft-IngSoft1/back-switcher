7292181212bab4b2303495e7bb005380bf2cb622
LucianoRojo <lucianorojo064@mi.unc.edu.ar>
2024-09-22 20:59:06 -0300

    Test de CreateGame y queries para la partida

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from router.game import game
 from router.cards import cards
 from models.base import Base, engine
 from models.game import GameTable
+from tests.gameCreation import testGameCreation
 
 # TODO -> Agregar los import con los modelos implementados, esto crea la tabla en la base de datos.
-
 
 app = FastAPI(
     title="Switcher - TuringSoft™",
@@ -21,3 +21,4 @@ app.include_router(cards)
 # Crea las tablas en base a los models importados.
 Base.metadata.create_all(bind=engine)
 
+testGameCreation()
--- models/game.py
+++ models/game.py
@@ -8,7 +8,7 @@ class GameTable(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     state = Column(String, default="Waiting", index=True)
-    turn = Column(Integer, default=0, index=True)
+    turn = Column(Integer, default=1, index=True)
     host = Column(String, index=True, nullable=False)
     players = Column(Integer, default=1, index=True)
     max_players = Column(Integer, default=4)
