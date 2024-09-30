f029a618330b6f8086a435dbe7fb7b0c5cb5bf68
valentinfntn <valentin.fantini@mi.unc.edu.ar>
2024-09-30 10:05:16 -0300

    Agregado test List Game y dropear base de datos para probar

--- tests/pregame_test.py
+++ tests/pregame_test.py
@@ -1,5 +1,11 @@
 import unittest
 import requests
+import json
+from http.client import responses
+
+from main import db
+
+db.teardown()
 
 class TestPregame(unittest.TestCase):
 
@@ -7,11 +13,11 @@ class TestPregame(unittest.TestCase):
 
         url = "http://localhost:8000/create_game"
         payload = {
-                    "game_name": "PartidaEjemplo",
-                    "owner_name": "UsuarioEjemplo",
-                    "min_player": 2,
-                    "max_player": 4
-                }
+            "game_name": "PartidaEjemplo",
+            "owner_name": "UsuarioEjemplo",
+            "min_player": 2,
+            "max_player": 4
+        }
         response = requests.post(url,json=payload)
         self.assertEqual(response.status_code,200)
 
@@ -25,8 +31,29 @@ class TestPregame(unittest.TestCase):
         self.assertEqual(response.status_code,422)
     
     def test_list_game(self):
-        pass
-        # TODO mocked si o si
+
+        url = "http://localhost:8000/list_games"
+        expected_json ={
+        "games_list": [
+            {
+                "id": 1,
+                "name": "PartidaEjemplo",
+                "state": "Waiting",
+                "turn": 0,
+                "host": 1,
+                "players": 1,
+                "max_players": 4,
+                "min_players": 2,
+                "password": "password",
+                "moves_deck": 50
+            }
+          ]
+        }
+        response = requests.get(url)
+        formatted_response = json.dumps(response.json(), sort_keys=True)
+        formatted_expected = json.dumps(expected_json, sort_keys=True)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(formatted_expected, formatted_response)
 
 if __name__ == '__main__':
     unittest.main()
