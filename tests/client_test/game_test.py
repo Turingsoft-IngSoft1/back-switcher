4cdd48e756c14b80ed20b50057b308bc8b04eb0d
valentinfntn <valentin.fantini@mi.unc.edu.ar>
2024-10-06 22:28:58 -0300

    Cambiado por testclient falta hacer mock correctamente

--- tests/client_test/game_test.py
+++ tests/client_test/game_test.py
@@ -1,58 +1,50 @@
-"""import json
-import pytest
-import requests
-import unittest
+import json,pytest
+from unittest.mock import patch
 
-from main import db
+@pytest.mark.order(0)
+def test_leave(mocker,test_db,client):
+    mocker.patch("utils.database.SERVER_DB", test_db)
 
+    url_create = "http://localhost:8000/create_game"
+    payload = {
+        "game_name": "PartidaEjemplo",
+        "owner_name": "UsuarioEjemplo",
+        "min_player": 2,
+        "max_player": 4
+    }
+    client.post(url_create, json=payload)
 
-class TestGame(unittest.TestCase):
-    @pytest.mark.order(0)
-    def test_leave(self):
-        db.teardown()
-        url_create = "http://localhost:8000/create_game"
-        payload = {
-            "game_name": "PartidaEjemplo",
-            "owner_name": "UsuarioEjemplo",
-            "min_player": 2,
-            "max_player": 4
-        }
-        requests.post(url_create, json=payload)
+    urljoin = "http://localhost:8000/join_game"
+    payload = {
+        "id_game": 1,
+        "player_name": "UsuarioParaLlenarLobby"
+    }
+    client.post(urljoin, json=payload)
 
-        urljoin = "http://localhost:8000/join_game"
-        payload = {
-            "id_game": 1,
-            "player_name": "UsuarioParaLlenarLobby"
-        }
-        requests.post(urljoin, json=payload)
+    url = "http://localhost:8000/leave_game"
+    payload = {
+        "id_player": 1,
+        "id_game": 1
+    }
+    response = client.post(url, json=payload)
 
-        url = "http://localhost:8000/leave_game"
-        payload = {
-            "id_player": 1,
-            "id_game": 1
-        }
-        response = requests.post(url, json=payload)
+    expected_json = {
+        "message": "Exit Successful."
+    }
 
-        expected_json = {
-            "message": "Exit Successful."
-        }
+    assert response.status_code == 200
+    formatted_response = json.dumps(response.json(), sort_keys=True)
+    formatted_expected = json.dumps(expected_json, sort_keys=True)
+    assert formatted_response == formatted_expected
 
-        self.assertEqual(response.status_code, 200)
-        formatted_response = json.dumps(response.json(), sort_keys=True)
-        formatted_expected = json.dumps(expected_json, sort_keys=True)
-        self.assertEqual(formatted_response, formatted_expected)
+@pytest.mark.order(1)
+def test_skip_turn(mocker,test_db,client):
+    mocker.patch("utils.database.SERVER_DB", test_db)
 
-    @pytest.mark.order(1)
-    def test_skip_turn(self):
-        url = "http://localhost:8000/skip_turn"
-        payload = {
-          "id_player": 2,
-          "id_game": 1
-        }
-        response = requests.post(url, json=payload)
-        self.assertEqual(response.status_code, 200)
-
-
-if __name__ == '__main__':
-    unittest.main()
-"""
+    url = "http://localhost:8000/skip_turn"
+    payload = {
+      "id_player": 2,
+      "id_game": 1
+    }
+    response = client.post(url, json=payload)
+    assert response.status_code == 200
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+from fastapi.testclient import TestClient
 from models import DBManager,BoardTable,GameTable,UserTable,MoveTable,FigureTable
+from main import app
 
 @pytest.fixture(scope='session')
 def test_db():
@@ -11,3 +13,8 @@ def test_db():
     #Teardown elimina todo registro de la base de datos, close cierra la session.
     TEST.teardown()
     TEST.close()
+
+@pytest.fixture
+def client():
+    with TestClient(app) as t_client:
+        yield t_client
