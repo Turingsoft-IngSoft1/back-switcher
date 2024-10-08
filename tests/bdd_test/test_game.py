9a2ef884782a2da74a2d934cffab92756873ed08
LucianoRojo <lucianorojo064@mi.unc.edu.ar>
2024-10-08 17:26:59 -0300

    Listos los tests de game

--- tests/bdd_test/test_game.py
+++ tests/bdd_test/test_game.py
@@ -1,14 +1,15 @@
 import pytest
 from querys import game_queries
 from models import GameTable
+from schemas.game_schema import Game
 from sqlite3 import IntegrityError
 
 def test_create_game(test_db):
     try:
         game_id = game_queries.create_game("Draken", 4, 2, test_db)
-        game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+        game = test_db.query(GameTable).filter_by(id=game_id).first()
         assert game is not None
-        assert game_id == game.id_game
+        assert game_id == game.id
         assert game.state == "Waiting"
         assert game.name == "Draken"
         assert game.min_players == 2
@@ -21,8 +22,8 @@ def test_create_game(test_db):
 def test_get_game(test_db):
     try:
         game_id = game_queries.create_game("Europas evija hjarta", 4, 2, test_db)
-        game = test_db.query(GameTable).filter_by(id_game=game_id).first()
-        assert game == game_queries.get_game(game_id, test_db)
+        game = test_db.query(GameTable).filter_by(id=game_id).first()
+        assert game_queries.get_game(game_id, test_db) == Game(id=1, name='Europas evija hjarta', state='Waiting', turn=0, host=0, players=1, max_players=4, min_players=2, password='password', moves_deck=50)
     except IntegrityError:
         pass
 
@@ -31,11 +32,11 @@ def test_set_game_state(test_db):
     game_id = game_queries.create_game("Genom Sverige i tiden", 3, 2, test_db)
         
     game_queries.set_game_state(game_id, "Playing", test_db)
-    game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+    game = test_db.query(GameTable).filter_by(id=game_id).first()
     assert game.state == "Playing"
         
     game_queries.set_game_state(game_id, "Finished", test_db)
-    game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+    game = test_db.query(GameTable).filter_by(id=game_id).first()
     assert game.state == "Finished"
 
     #Caso de error: Setear un estado inválido
@@ -49,7 +50,7 @@ def test_get_game_state(test_db):
     try:
         game_id = game_queries.create_game("Västkustens styrka och stolthet", 3, 2, test_db)
         game_queries.set_game_state(game_id, "Playing", test_db)
-        game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+        game = test_db.query(GameTable).filter_by(id=game_id).first()
         assert game.state == "Playing"
     except IntegrityError:
         pass
@@ -60,25 +61,25 @@ def test_listing_games(test_db): #Chequear
         game_id2 = game_queries.create_game("Skaraborgs regemente", 4, 2, test_db)
         game_id3 = game_queries.create_game("Uppland regemente", 2, 2, test_db)
         games = test_db.query(GameTable).all()
-        assert games == game_queries.listing_games(test_db)
+        assert game_queries.listing_games(test_db) == [Game(id=1, name='Gottland regemente', state='Waiting', turn=0, host=0, players=1, max_players=3, min_players=2, password='password', moves_deck=50), Game(id=2, name='Skaraborgs regemente', state='Waiting', turn=0, host=0, players=1, max_players=4, min_players=2, password='password', moves_deck=50), Game(id=3, name='Uppland regemente', state='Waiting', turn=0, host=0, players=1, max_players=2, min_players=2, password='password', moves_deck=50)]
     except IntegrityError:
         pass
 
 def test_set_game_turn(test_db):
     
     game_id = game_queries.create_game("Scania regemente", 3, 2, test_db)
-    game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+    game = test_db.query(GameTable).filter_by(id=game_id).first()
         
     game_queries.set_game_turn(game_id, 2, test_db)
-    game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+    game = test_db.query(GameTable).filter_by(id=game_id).first()
     assert game.turn == 2
         
     game_queries.set_game_turn(game_id, 3, test_db)
-    game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+    game = test_db.query(GameTable).filter_by(id=game_id).first()
     assert game.turn == 3
         
     game_queries.set_game_turn(game_id, 1, test_db)
-    game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+    game = test_db.query(GameTable).filter_by(id=game_id).first()
     assert game.turn == 1
     
     try:
@@ -107,7 +108,7 @@ def test_get_game_turn(test_db):
 def test_set_game_host(test_db):
     try:
         game_id = game_queries.create_game("Jämtland regemente", 3, 2, test_db)
-        game = test_db.query(GameTable).filter_by(id_game=game_id).first()
+        game = test_db.query(GameTable).filter_by(id=game_id).first()
         game_queries.set_game_host(game_id, 2, test_db)
         assert game.host == 2
         game_queries.set_game_host(game_id, 3, test_db)
@@ -165,11 +166,11 @@ def test_remove_player(test_db):
     game_queries.add_player(game_id, test_db)
     game_queries.add_player(game_id, test_db)
     game_queries.remove_player(game_id, test_db)
+    assert game_queries.get_players(game_id, test_db) == 3
+    game_queries.remove_player(game_id, test_db)
     assert game_queries.get_players(game_id, test_db) == 2
     game_queries.remove_player(game_id, test_db)
     assert game_queries.get_players(game_id, test_db) == 1
-    game_queries.remove_player(game_id, test_db)
-    assert game_queries.get_players(game_id, test_db) == 0
     
     try:
         #Caso de error: Intentar sacar a un jugador de una partida vacía.
@@ -182,6 +183,6 @@ def test_remove_game(test_db):
     try:
         game_id = game_queries.create_game("Gotlands regemente", 3, 2, test_db)
         game_queries.remove_game(game_id, test_db)
-        assert test_db.query(GameTable).filter_by(id_game=game_id).first() is None
+        assert test_db.query(GameTable).filter_by(id=game_id).first() is None
     except IntegrityError:
         pass
