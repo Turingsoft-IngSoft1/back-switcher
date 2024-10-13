c1bfcccaa2268b0bf86b8df364a5938d87571904
valetinfntn <valentin.fantini@mi.unc.edu.ar>
2024-10-13 14:22:11 -0300

    usar movimiento/faltan test

--- router/cards.py
+++ router/cards.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
 from querys.move_queries import *
 from querys.game_queries import *
+from querys.figure_queries import *
+from querys.board_queries import *
+
 from schemas.response_models import *
-from querys import get_revealed_figures, refill_revealed_figures, figures_in_hand
+from schemas.move_schema import Move
 from utils.ws import manager
 from utils.database import SERVER_DB
 
@@ -25,13 +30,26 @@ async def get_moves(id_player: int, id_game: int):
 
 
 @cards.post("/use_moves")
-def use_moves(id_player: int, id_game: int):
+async def use_moves(e: EntryMove):
     """Usar una carta de movimiento."""
-
-    # TODO Implementacion ->
-
-    return {"Movimientos Usados Correctamente."}
-
+    move = Move(name=e.name, initial_position=e.pos1)
+    if (e.name not in get_hand(e.id_game, e.id_player, SERVER_DB)):
+        raise HTTPException(status_code=400, detail="El usuario no tiene ese movimiento.")
+    if (e.pos2 not in move.available_moves):
+        raise HTTPException(status_code=400, detail="Movimiento invalido.")
+    use_move(e.id_game, e.id_player, e.name, SERVER_DB)
+    try:
+        board = get_board(e.id_game, SERVER_DB)
+        board[e.pos1[1]][e.pos1[0]], board[e.pos2[1]][e.pos2[0]] = (
+            board[e.pos2[1]][e.pos2[0]],
+            board[e.pos1[1]][e.pos1[0]],
+        )
+        update_board(e.id_game, board, SERVER_DB)
+    except BoardValidationError:
+        return JSONResponse(status_code=400, content={"detail": "Error al actualizar el tablero."})
+        
+    await manager.broadcast("REFRESH_BOARD",e.id_game)
+    return {"Movimiento Realizado Correctamente."}
 
 @cards.get("/get_figure/{id_game}/{id_player}")
 async def get_figure(id_player: int, id_game: int):
--- schemas/move_schema.py
+++ schemas/move_schema.py
@@ -1,10 +1,79 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator, model_validator
+from models.move import valid_moves
+
+
+def generate_moves(name: str, initial_position: tuple[int, int]) -> list[tuple[int, int]]:
+    """Genera movimientos disponibles en función del nombre y la posición inicial."""\
+    """Sanitiza movimientos fuera de límites establecidos por el tablero."""
+    available_moves = []
+    match name:
+        case "mov1":
+            available_moves.extend([
+                (initial_position[0] + 2, initial_position[1] + 2),
+                (initial_position[0] + 2, initial_position[1] - 2),
+                (initial_position[0] - 2, initial_position[1] + 2),
+                (initial_position[0] - 2, initial_position[1] - 2)
+            ])
+        case "mov2":
+            available_moves.extend([
+                (initial_position[0], initial_position[1] + 2),
+                (initial_position[0], initial_position[1] - 2),
+                (initial_position[0] + 2, initial_position[1]),
+                (initial_position[0] - 2, initial_position[1])
+            ])
+        case "mov3":
+            available_moves.extend([
+                (initial_position[0], initial_position[1] + 1),
+                (initial_position[0], initial_position[1] - 1),
+                (initial_position[0] + 1, initial_position[1]),
+                (initial_position[0] - 1, initial_position[1])
+            ])
+        case "mov4":
+            available_moves.extend([
+                (initial_position[0] + 1, initial_position[1] + 1),
+                (initial_position[0] + 1, initial_position[1] - 1),
+                (initial_position[0] - 1, initial_position[1] + 1),
+                (initial_position[0] - 1, initial_position[1] - 1)
+            ])
+        case "mov5":
+            available_moves.extend([
+                (initial_position[0] + 1, initial_position[1] + 3),
+                (initial_position[0] - 3, initial_position[1] + 1),
+                (initial_position[0] - 1, initial_position[1] - 3),
+                (initial_position[0] - 3, initial_position[1] + 1)
+            ])
+        case "mov6":
+            available_moves.extend([
+                (initial_position[0] - 1, initial_position[1] + 3),
+                (initial_position[0] - 3, initial_position[1] - 1),
+                (initial_position[0] + 1, initial_position[1] - 3),
+                (initial_position[0] + 1, initial_position[1] + 3)
+            ])
+        case "mov7":
+            available_moves.extend([
+                (initial_position[0], initial_position[1] + 4),
+                (initial_position[0], initial_position[1] - 4),
+                (initial_position[0] + 4, initial_position[1]),
+                (initial_position[0] - 4, initial_position[1])
+            ])
+
+    valid_moves_in_bounds = [
+        (x, y) for x, y in available_moves if 0 <= x < 6 and 0 <= y < 6
+    ]
+    
+    return valid_moves_in_bounds
 
 
 class Move(BaseModel):
     """Schema for move cards implementation"""
-    id: int = Field(description="Unique integer that specifies this move.")
-    name: str = Field(min_length=1, max_length=100, description="Name of the move.")
-    status: str = Field(min_length=1, max_length=100, description="Status the card is in (Deck, InHand, Played or Discarded).")
-    user_id: int = Field(description="Unique integer that specifies the owner of the card.")
-    id_game: int = Field(description="Unique integer that specifies the game the card is in.")
+    name: str = Field(min_length=4, max_length=4, description="Name of the move.")
+    initial_position: tuple[int, int] = Field(description="Initial position of the move.")
+    available_moves: list[tuple[int, int]] = Field(default_factory=list, description="List of possible moves.")
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.name not in valid_moves:
+            raise ValueError("Invalid move name")
+        if not (0 <= self.initial_position[0] < 6 and 0 <= self.initial_position[1] < 6):
+            raise ValueError("Invalid initial position")
+        self.available_moves = generate_moves(self.name, self.initial_position)
