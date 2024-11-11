6feb7fad8c6e61c09e69fa3d5fba366f995f5d13
valetinfntn <valentin.fantini@mi.unc.edu.ar>
2024-11-10 22:43:51 -0300

    agregado test

--- tests/client_test/profile_test.py
+++ tests/client_test/profile_test.py
@@ -1,11 +1,34 @@
 import json
 
-def test_new_profile(client):
+def test_profile(client):
     #Crear PartidaEjemplo y UsuarioEjemplo. 
-    url = "http://localhost:8000/create_game"
+    url_profile = "http://localhost:8000/new_profile"
+    response = client.get(url_profile)
+    user_profile = response.text
+
+    assert user_profile is not None
+
+    url_create = f"http://localhost:8000/create_game?profile_id={user_profile}"
     payload = {
-        "game_name": "PartidaEjemplo",
-        "owner_name": "UsuarioEjemplo",
+        "game_name": "Partida",
+        "owner_name": "Usuario1",
         "min_player": 2,
-        "max_player": 2
+        "max_player": 2,
+        "password": ""
     }
+    client.post(url_create, json=payload)
+
+    url = "http://localhost:8000/join_game"
+    payload = {
+        "id_game": 1,
+        "player_name": "Usuario2",
+        "password": ""
+    }
+    client.post(url_create, json=payload)
+
+    url = "http://localhost:8000/start_game/1"
+    client.post(url)
+
+    url_load = f"http://localhost:8000/load_profile/{user_profile}"
+    user_games = client.get(url_load)
+    assert user_games[0].id_game == 1
